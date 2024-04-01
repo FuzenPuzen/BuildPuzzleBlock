07ae85b3db8f507df393ea76a720d2ba2c5db2bf
FuzenPuzen <angel017791@gmail.com>
2024-04-01 14:58:03 +0300

    ChangeCashe Polici

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "DefaultCompany-New Try-1.0.2";
-const contentToCache = [
-    "Build/BuildPuzzleBlock.loader.js",
-    "Build/BuildPuzzleBlock.framework.js",
-    "Build/BuildPuzzleBlock.data",
-    "Build/BuildPuzzleBlock.wasm",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
